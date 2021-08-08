a15eaf6ab05384f9b4358325d9ce67a187b18bb7
Kim TaeWon <kimtae9217@gmail.com>
2021-08-08 15:16:59 +0900

    오타 수정 및 DIP 원칙 해결..?

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -5,12 +5,12 @@ import hello.core.member.Member;
 
 public class FixDiscountPolicy implements DiscountPolicy{
 
-    private int discountFIxAmount = 1000; //1000원 할인
+    private int discountFixAmount = 1000; //1000원 할인
 
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
-            return discountFIxAmount;
+            return discountFixAmount;
         } else{
             return 0;
         }
