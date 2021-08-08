05cf777d5fbeb21ddee9580f2b8660c2867f1f07
Kim TaeWon <kimtae9217@gmail.com>
2021-08-08 16:03:59 +0900

    [feat] DIP 원칙 해결과 의존성 주입

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -10,8 +10,10 @@ import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
@@ -19,6 +21,5 @@ public class OrderApp {
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         System.out.println("order = " + order);
-        System.out.println("order");
     }
 }
